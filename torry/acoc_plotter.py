f6d1cec772395df4c3162cf665b39807d873c070
Torry Tufteland <torry.tufteland@gmail.com>
2015-10-12 10:06:43 +0200

    Splitted up main acoc file and various refactorings

--- torry/acoc_plotter.py
+++ torry/acoc_plotter.py
@@ -26,3 +26,27 @@ class LivePheromonePlot:
             plt.setp(self.plot_lines[j], linewidth=edge.pheromone_strength)
         plt.draw()
         plt.pause(0.01)
+
+
+def plot_path_lengths(ant_paths):
+    x_coord = range(len(ant_paths))
+    path_lengths = [len(p) for p in ant_paths]
+    y_coord = path_lengths
+    plt.plot(x_coord, y_coord, 'k-')
+    plt.axis([0, len(ant_paths), 0, max(path_lengths)])
+    plt.show()
+
+
+def plot_path(path, matrix):
+    for edge in path:
+        plt.plot([edge.a_vertex[0], edge.b_vertex[0]], [edge.a_vertex[1], edge.b_vertex[1]], 'k-')
+    plt.axis([-1, matrix.x_size, -1, matrix.y_size])
+    plt.show()
+
+
+def plot_pheromone_values(matrix):
+    for edge in matrix.edges:
+        line = plt.plot([edge.a_vertex[0], edge.b_vertex[0]], [edge.a_vertex[1], edge.b_vertex[1]], 'k-')
+        plt.setp(line, linewidth=edge.pheromone_strength)
+    plt.axis([-1, matrix.x_size, -1, matrix.y_size])
+    plt.show()
---
+++ torry/ant.py
@@ -0,0 +1,4 @@
+class Ant:
+    def __init__(self, start_vertex):
+        self.current_vertex = start_vertex
+        self.edges_travelled = []

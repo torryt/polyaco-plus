95c306ac6633e66164a67357e6b9ad91ee5651cc
Torry Tufteland <torry.tufteland@gmail.com>
2015-11-05 09:59:26 +0100

    More reuseable parameter test

--- experiments/parameter_tester.py
+++ experiments/parameter_tester.py
@@ -22,21 +22,24 @@ default_config = {
     'live_plot': False
 }
 
-
-red = np.insert(dg.uniform_rectangle((1, 4), (2, 5), 500), 2, 0, axis=0)
-blue = np.insert(dg.uniform_rectangle((5, 8), (2, 5), 500), 2, 1, axis=0)
+red = dg.uniform_rectangle((1, 4), (2, 5), 500, 0)
+blue = dg.uniform_rectangle((5, 8), (2, 5), 500, 1)
 data = np.concatenate((red, blue), axis=1)
 line_shapes = ['b-', 'g^', 'r-', 'c-', 'm-', 'y-']
 
 
-def run(ant_count=):
-    classifier = acoc.Classifier(_ant_count, q, _q_max, q_min, q_init, rho, _alpha, beta, ant_init)
-    all_ant_scores = np.zeros((_iterations, _ant_count))
+def run(*args):
+    config = dict(default_config)
+    for arg in args:
+        config[arg[0]] = arg[1]
+    iterations = config['iterations']
+    classifier = acoc.Classifier(config)
+    all_ant_scores = np.zeros((iterations, config['ant_count']))
 
     for i in range(iterations):
-        utils.print_on_current_line("Iteration: {}/{}".format(i + 1, _iterations))
+        utils.print_on_current_line("Iteration: {}/{}".format(i + 1, iterations))
         ant_scores, path = \
-            classifier.classify(data, _live_plot)
+            classifier.classify(data, config['live_plot'])
         all_ant_scores[i, :] = ant_scores
 
     return all_ant_scores.mean(0)
@@ -48,7 +51,7 @@ def parameter_tester(parameter_name, values):
     all_scores = []
     for index, v in enumerate(values):
         print("\nRun {} with value {}".format(index+1, v))
-        scores = run()
+        scores = run((parameter_name, v))
         line = plt.plot(range(len(scores)), scores, line_shapes[index], label=parameter_name + '=' + str(v))
         plots.append(line)
         all_scores.append(scores)
@@ -59,75 +62,7 @@ def parameter_tester(parameter_name, values):
     acoc_plotter.save_plot()
 
 
-def test_ant_init():
-    plt.clf()
-    plots = []
-    value_tag = 'ant_init'
-    values = ['random', 'weighted', 'static']
-    all_scores = []
-    for index, v in enumerate(values):
-        print("\nRun {} with value {}".format(index+1, v))
-        scores = run(ant_init=v)
-        line = plt.plot(range(len(scores)), scores, line_shapes[index], label=value_tag+'='+str(v))
-        plots.append(line)
-        all_scores.append(scores)
-
-    utils.save_object(all_scores, value_tag)
-    plt.legend()
-    plt.axis([0, len(scores), 0, 1])
-    acoc_plotter.save_plot()
-
-
-def test_q_init():
-    plt.clf()
-    plots = []
-    value_tag = 'q_init'
-    values = ['random', 'weighted', 'static']
-    all_scores = []
-    for index, v in enumerate(values):
-        print("\nRun {} with value {}".format(index+1, v))
-        scores = run(ant_init=v)
-        line = plt.plot(range(len(scores)), scores, line_shapes[index], label=value_tag+'='+str(v))
-        plots.append(line)
-        all_scores.append(scores)
-
-    utils.save_object(all_scores, value_tag)
-    plt.legend()
-    plt.axis([0, len(scores), 0, 1])
-    acoc_plotter.save_plot()
-
-
-def test_rho():
-    plt.clf()
-    plots = []
-    value_tag = 'rho'
-    values = [0.001, 0.01, 0.02, 0.1, 0.3]
-    print("Testing parameter " + value_tag)
-    for index, v in enumerate(values):
-        print("\nRun {} with value {}".format(index+1, v))
-        scores = run(rho=v)
-        line = plt.plot(range(len(scores)), scores, line_shapes[index], label=value_tag+'='+str(v))
-        plots.append(line)
-
-    plt.legend()
-    plt.axis([0, len(scores), 0, 1])
-    acoc_plotter.save_plot()
-
-
-def test_iterations():
-    plt.clf()
-    plots = []
-    value_tag = 'iterations'
-    values = [1, 2, 5, 10]
-    print("Testing parameter " + value_tag)
-    for index, v in enumerate(values):
-        print("\nRun {} with value {}".format(index+1, v))
-        scores = run(iterations=v)
-        line = plt.plot(range(len(scores)), scores, line_shapes[index], label=value_tag+'='+str(v))
-        plots.append(line)
-
-    plt.legend()
-    plt.axis([0, len(scores), 0, 1])
-    acoc_plotter.save_plot()
-
-test_q_init()
+# parameter_tester('ant_init', ['random', 'weighted', 'static'])
+parameter_tester('q_init', [default_config['q_max'], default_config['q_min']])
+# parameter_tester('rho', [0.001, 0.01, 0.02, 0.1, 0.3])
+# parameter_tester('iterations', [1, 2, 5, 10])

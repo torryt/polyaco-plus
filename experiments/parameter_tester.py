ab13e2b16f8ff522472f75afad704dc45b026864
Torry Tufteland <torry.tufteland@gmail.com>
2016-04-01 15:38:17 +0200

    BC and IRIS experiment

--- experiments/parameter_tester.py
+++ experiments/parameter_tester.py
@@ -4,8 +4,10 @@ from copy import copy
 from sklearn.cross_validation import train_test_split
 import sys
 import matplotlib
+
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
+
 sys.path.append(osp.dirname(osp.dirname(osp.abspath(__file__))))
 
 import acoc
@@ -13,8 +15,9 @@ import utils
 from utils import data_manager
 from config import SAVE_DIR, CLASSIFIER_CONFIG
 
-CLASSIFIER_CONFIG.runs = 50
-CLASSIFIER_CONFIG.data_set = 'iris'
+CLASSIFIER_CONFIG.runs = 100
+CLASSIFIER_CONFIG.max_level = 3
+CLASSIFIER_CONFIG.level_convergence_rate = 1200
 
 
 def run(*args):
@@ -38,11 +41,11 @@ def run(*args):
         predictions = clf.evaluate(X_test)
 
         classifier_scores.append(acoc.compute_score(predictions, y_test))
-    return sum(classifier_scores) / config.runs
+    return classifier_scores
 
 
 def parameter_tester(parameter_name, values, config=CLASSIFIER_CONFIG):
-    save_folder = datetime.utcnow().strftime('%Y-%m-%d_%H%M') + '_' + config.data_set
+    save_folder = datetime.utcnow().strftime('%Y-%m-%d_%H%M')
     iterator = 0
     full_path = osp.join(SAVE_DIR, save_folder) + '-' + str(iterator)
     while osp.exists(full_path):
@@ -52,31 +55,21 @@ def parameter_tester(parameter_name, values, config=CLASSIFIER_CONFIG):
     print("\n\nExperiment for parameter '{}' with values {}".format(parameter_name, values))
 
     plt.clf()
-    scores = []
+    all_scores = []
     for index, v in enumerate(values):
-        print("Run {} with value {}".format(index+1, v))
-        score = run((parameter_name, v))
-        scores.append(score)
+        print("Run {} with value {}".format(index + 1, v))
+        scores = run((parameter_name, v))
+        all_scores.append(scores)
         utils.print_on_current_line('')
     utils.save_dict(config, save_folder, 'config_' + parameter_name + '.txt')
     header = ','.join(str(s) for s in values)
-    result_str = header + '\n' + ','.join(["{:.4f}".format(s) for s in scores])
+    result_str = header + '\n' + ','.join(["{:.4f}".format(sum(s) / CLASSIFIER_CONFIG.runs) for s in all_scores]) + \
+                 '\n\n' + 'all scores:\n'
+
+    for a in all_scores:
+        result_str += ','.join('{:.4f}'.format(s) for s in a) + '\n'
     utils.save_string_to_file(result_str, parent_folder=save_folder, file_name='result.txt')
 
 
 if __name__ == "__main__":
-    CLASSIFIER_CONFIG.data_set = 'iris'
-    CLASSIFIER_CONFIG.level_convergence_rate = 100
-    parameter_tester('one_less_class', [True, False])
-    CLASSIFIER_CONFIG.level_convergence_rate = 200
-    parameter_tester('one_less_class', [True, False])
-    CLASSIFIER_CONFIG.level_convergence_rate = 400
-    parameter_tester('one_less_class', [True, False])
-
-    CLASSIFIER_CONFIG.data_set = 'breast_cancer'
-    CLASSIFIER_CONFIG.level_convergence_rate = 100
-    parameter_tester('one_less_class', [True, False])
-    CLASSIFIER_CONFIG.level_convergence_rate = 200
-    parameter_tester('one_less_class', [True, False])
-    CLASSIFIER_CONFIG.level_convergence_rate = 400
-    parameter_tester('one_less_class', [True, False])
+    parameter_tester('data_set', ['iris', 'breast_cancer'])
